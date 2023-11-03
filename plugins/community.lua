e40089da89e26e0429552175af93b2bc320feb3a
Suvrajit Ray <suray@microsoft.com>
2023-11-03 17:43:51 +0530

    community plugins added.

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.html-css" },
 }
